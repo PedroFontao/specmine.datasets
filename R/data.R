f061f8156723399a3c0e9182b14bdfd9b1b66ba4
Pedro Fontão <pedrofontao16@gmail.com>
2026-07-06 10:33:12 +0100

    Clean dataset documentation for CRAN resubmission

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' Cachexia is a complex metabolic syndrome associated with an underlying illness
 #' (such as cancer) and characterized by loss of muscle with or without loss of
 #' fat mass (Evans et al., 2008). A total of 77 urine samples were collected,
-#' being 47 of them patients with cachexia and 30 control patients.
+#' with 47 from patients with cachexia and 30 from control patients.
 #'
 #' @format An object of class \code{"list"}
 #'
@@ -14,13 +14,13 @@
 #' Metabolomics 7:25-34.
 "cachexia"
 
-#' Cassava Postharvest Physiological Deterioration
+#' Cassava postharvest physiological deterioration
 #'
-#' Cassava is a root well known and widely cultivated in tropical and
-#' subtropical regions for its starchy tuberous root, which is a great
-#' source of carbohydrates. It also has a great variety of applications,
-#' like animal feeding, culinary or alcoholic beverages. In some countries,
-#' cassava has also been tested as an ethanol biofuel feedstock.
+#' Cassava is a root widely cultivated in tropical and subtropical regions for
+#' its starchy tuberous root, which is an important source of carbohydrates.
+#' It has a wide variety of applications, including animal feeding, culinary
+#' uses and alcoholic beverages. In some countries, cassava has also been
+#' tested as an ethanol biofuel feedstock.
 #'
 #' @format An object of class \code{"list"}
 #'
@@ -29,104 +29,98 @@
 #' roots during postharvest physiological deterioration. Food Chemistry 161:67-78.
 "cassavaPPD"
 
-#' Brazilian Propolis from Different Harvest Seasons and Different Agroecological Regions (dataset)
+#' Brazilian propolis from different harvest seasons and agroecological regions (dataset)
 #'
-#' @description Propolis or bee glue is a sticky dark-colored substance produced
-#' from the collected buds or exudates of plants (resin) by bees
-#' (Apis mellifera L.). The resin is masticated, salivary enzymes are
-#' added, and the partially digested material is mixed with beewax
-#' and used in the hive to seal the walls, strengthen the borders of
-#' combs, and embalm dead invaders (Wollenweber et al., 1990).
-#' The propolis samples are from NMR data and were collected in the
-#' autumn (AU), winter (WI), spring (SP), and summer (SM) of 2010
-#' from Apis mellifera hives located in southern Brazil (Santa
-#' Catarina State). A total of 59 samples were collected, and the
-#' distribution of samples by seasons being: SM - 16 samples, AU
-#' and SP - 15 samples, WI - 13 samples. Also, three agroecological
-#' regions were defined for the different apiaries, and one
-#' distributed as follows: Highlands - 12 samples, Plain - 11 samples,
-#' Plateau - 36 samples.
+#' @description Propolis, or bee glue, is a sticky dark-colored substance produced
+#' from collected buds or plant exudates (resin) by bees (Apis mellifera L.).
+#' The resin is masticated, salivary enzymes are added, and the partially
+#' digested material is mixed with beeswax and used in the hive to seal walls,
+#' strengthen comb borders and embalm dead invaders (Wollenweber et al., 1990).
+#' The propolis samples are from NMR data and were collected in autumn (AU),
+#' winter (WI), spring (SP) and summer (SM) of 2010 from Apis mellifera hives
+#' located in southern Brazil (Santa Catarina State). A total of 59 samples
+#' were collected, distributed by season as follows: SM - 16 samples, AU and
+#' SP - 15 samples each, and WI - 13 samples. Three agroecological regions were
+#' defined for the different apiaries and distributed as follows: Highlands -
+#' 12 samples, Plain - 11 samples and Plateau - 36 samples.
 #'
 #' @format An object of class \code{"list"}
 #'
 #' @references E. Wollenweber, B. M. Hausen, and W. Greenaway. Phenolic
-#' constituents and sensitizing properties of propolis, poplar
-#' balsam and balsam of peru. Bulletin de Groupe Polyphenol,
-#' 15:112-120, 1990.
+#' constituents and sensitizing properties of propolis, poplar balsam and
+#' balsam of Peru. Bulletin de Groupe Polyphenol, 15:112-120, 1990.
+#'
 #' M. Maraschin, A. Somensi-Zeggio, S. K. Oliveira, S. Kuhnen,
 #' M. M. Tomazzoli, A. C. M. Zeri, R. Carreira, and M. Rocha.
-#' A machine learning and chemometrics assisted interpretation
-#' of spectroscopic data - a nmr-based metabolomics platform for
-#' the assessment of brazilian propolis. 2012.
+#' A machine learning and chemometrics assisted interpretation of spectroscopic
+#' data - a NMR-based metabolomics platform for the assessment of Brazilian
+#' propolis. 2012.
 "propolis"
 
-#' Brazilian Propolis from Different Harvest Seasons and Different Agroecological Regions (sample list)
+#' Brazilian propolis from different harvest seasons and agroecological regions (sample list)
 #'
-#' @description Propolis or bee glue is a sticky dark-colored substance produced
-#' from the collected buds or exudates of plants (resin) by bees
-#' (Apis mellifera L.). The resin is masticated, salivary enzymes are
-#' added, and the partially digested material is mixed with beewax
-#' and used in the hive to seal the walls, strengthen the borders of
-#' combs, and embalm dead invaders (Wollenweber et al., 1990).
-#' The propolis samples are from NMR data and were collected in the
-#' autumn (AU), winter (WI), spring (SP), and summer (SM) of 2010
-#' from Apis mellifera hives located in southern Brazil (Santa
-#' Catarina State). A total of 59 samples were collected, and the
-#' distribution of samples by seasons being: SM - 16 samples, AU
-#' and SP - 15 samples, WI - 13 samples. Also, three agroecological
-#' regions were defined for the different apiaries, and one
-#' distributed as follows: Highlands - 12 samples, Plain - 11 samples,
-#' Plateau - 36 samples.
+#' @description Propolis, or bee glue, is a sticky dark-colored substance produced
+#' from collected buds or plant exudates (resin) by bees (Apis mellifera L.).
+#' The resin is masticated, salivary enzymes are added, and the partially
+#' digested material is mixed with beeswax and used in the hive to seal walls,
+#' strengthen comb borders and embalm dead invaders (Wollenweber et al., 1990).
+#' The propolis samples are from NMR data and were collected in autumn (AU),
+#' winter (WI), spring (SP) and summer (SM) of 2010 from Apis mellifera hives
+#' located in southern Brazil (Santa Catarina State). A total of 59 samples
+#' were collected, distributed by season as follows: SM - 16 samples, AU and
+#' SP - 15 samples each, and WI - 13 samples. Three agroecological regions were
+#' defined for the different apiaries and distributed as follows: Highlands -
+#' 12 samples, Plain - 11 samples and Plateau - 36 samples.
 #'
 #' @format An object of class \code{"list"}
 #'
 #' @references E. Wollenweber, B. M. Hausen, and W. Greenaway. Phenolic
-#' constituents and sensitizing properties of propolis, poplar
-#' balsam and balsam of peru. Bulletin de Groupe Polyphenol,
-#' 15:112-120, 1990.
+#' constituents and sensitizing properties of propolis, poplar balsam and
+#' balsam of Peru. Bulletin de Groupe Polyphenol, 15:112-120, 1990.
+#'
 #' M. Maraschin, A. Somensi-Zeggio, S. K. Oliveira, S. Kuhnen,
 #' M. M. Tomazzoli, A. C. M. Zeri, R. Carreira, and M. Rocha.
-#' A machine learning and chemometrics assisted interpretation
-#' of spectroscopic data - a nmr-based metabolomics platform for
-#' the assessment of brazilian propolis. 2012.
+#' A machine learning and chemometrics assisted interpretation of spectroscopic
+#' data - a NMR-based metabolomics platform for the assessment of Brazilian
+#' propolis. 2012.
 "propolisSampleList"
 
 #' Mouse spinal cord LC-MS data
 #'
 #' @description This dataset consists of 12 LC-MS spectra samples in netCDF
-#' format from mouse spinal cord, divided into 2 groups: the wild type
-#' and the knockout group. The data was obtained from MetaboAnalyst and
-#' originates from a study describing a general strategy for identifying
-#' endogenous substrates of enzymes by untargeted LC-MS analysis of tissue
-#' metabolomes from wild-type and enzyme-inactivated organisms.
+#' format from mouse spinal cord, divided into two groups: wild type and
+#' knockout. The data was obtained from MetaboAnalyst and originates from a
+#' study describing a general strategy for identifying endogenous substrates of
+#' enzymes by untargeted LC-MS analysis of tissue metabolomes from wild-type and
+#' enzyme-inactivated organisms.
 #'
 #' @format An object of class \code{"list"}
 #'
-#' @references A. Saghatelian, S.A. Trauger, E.J. Want, E.G. Hawkins, G. Siuzdak,
-#' B.F. Cravatt. Assignment of endogenous substrates to enzymes
+#' @references A. Saghatelian, S.A. Trauger, E.J. Want, E.G. Hawkins,
+#' G. Siuzdak, and B.F. Cravatt. Assignment of endogenous substrates to enzymes
 #' by global metabolite profiling. Biochemistry, 43:14332-14339, 2004.
 "spinalCord"
 
-#' Information on the library of NMR reference spectra in specmine package
+#' Information on the library of NMR reference spectra in the specmine package
 #'
-#' @description This dataset provides all the information on the library
-#' of NMR spectra used as references in NMR metabolite identification.
+#' @description This dataset provides information on the library of NMR spectra
+#' used as references in NMR metabolite identification.
 #'
 #' @format A data frame with 1816 observations on the following 9 variables.
-#' Each observation corresponds to a spectrum in our library.
+#' Each observation corresponds to a spectrum in the library.
 #' \describe{
-#' \item{\code{SPCMNS}}{a character vector with the spectra IDs.}
-#' \item{\code{SPCMNM}}{a character vector with the metabolite IDs of the corresponding spectra.}
-#' \item{\code{FREQUENCY}}{a character vector with the frequencies under which the spectra were obtained.}
-#' \item{\code{NUCLEUS}}{a character vector mentioning the nucleus examined. All observations are '1H'.}
-#' \item{\code{PH}}{a character vector with the pH of the samples from which the spectra were obtained. May contain missing values.}
-#' \item{\code{TEMPERATURE}}{a character vector with the temperature under which the spectra were obtained. May contain missing values.}
-#' \item{\code{SOLVENT}}{a character vector with the solvent of the samples from which the spectra were obtained.}
-#' \item{\code{ORIGINAL_DATABASE_ID}}{whenever available, a character vector with the ID of the corresponding spectra from the database where it was originally acquired.}
-#' \item{\code{DATABASE}}{a character vector specifying from which database the spectra were taken.}
+#' \item{\code{SPCMNS}}{A character vector with the spectra IDs.}
+#' \item{\code{SPCMNM}}{A character vector with the metabolite IDs of the corresponding spectra.}
+#' \item{\code{FREQUENCY}}{A character vector with the frequencies at which the spectra were obtained.}
+#' \item{\code{NUCLEUS}}{A character vector indicating the examined nucleus. All observations are '1H'.}
+#' \item{\code{PH}}{A character vector with the pH of the samples from which the spectra were obtained. May contain missing values.}
+#' \item{\code{TEMPERATURE}}{A character vector with the temperature at which the spectra were obtained. May contain missing values.}
+#' \item{\code{SOLVENT}}{A character vector with the solvent of the samples from which the spectra were obtained.}
+#' \item{\code{ORIGINAL_DATABASE_ID}}{Whenever available, a character vector with the ID of the corresponding spectrum from the database where it was originally acquired.}
+#' \item{\code{DATABASE}}{A character vector specifying the database from which the spectra were taken.}
 #' }
 #'
-#' @references The spectra were taken from the following databases: HMDB,
-#' BMRB and SDBS. Some spectra were internally acquired and are mentioned
-#' as OUR in the DATABASE variable.
+#' @references The spectra were taken from the HMDB, BMRB and SDBS databases.
+#' Some spectra were internally acquired and are mentioned as OUR in the
+#' \code{DATABASE} variable.
 "spectra_options"
